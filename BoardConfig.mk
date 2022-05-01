2e4a2c74b568b841cae3b18f1de5d62187a55d12
Ricky Cheung <rcheung844@gmail.com>
2022-05-01 12:30:38 +0800

    w701: Import prebuilt kernel and kernel headers
    Change-Id: I0c8f70a58e03223b28baf7a8fb73c68d33f1a120

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,8 +93,10 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/zImage-dtb
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
