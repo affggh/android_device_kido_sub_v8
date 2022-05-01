d53dbc24d0e38643c8c7dc3c3dfb0ef09b5591a5
Ricky Cheung <rcheung844@gmail.com>
2022-05-01 16:31:27 +0800

    w701: gps: Copy GPS configs
    Change-Id: I44faf09d9b19f86c0eb4a763748d2af905f1ec53

--- device.mk
+++ device.mk
@@ -77,6 +77,15 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
