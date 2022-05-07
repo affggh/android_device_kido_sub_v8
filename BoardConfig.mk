d5b83ab6c6ab755a0ba0d51321a92521441b1e08
Ricky Cheung <rcheung844@gmail.com>
2022-05-07 22:50:13 +0800

    w701: Cleanup cmdline
    * Enforcing time
    * Disable earlyprintk
    
    Change-Id: I27a716ed1f94a88ff4fd228ceae5bc6139763194

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,8 +109,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8909
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
